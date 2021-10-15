3bf9d3c65deda3e53f1092b7dbe8cc34345fad64
Matheus Fillipe <67832413+toyc497@users.noreply.github.com>
2021-10-15 17:17:46 -0300

    Versao 2
    Falta retornar o array ao chegar em Z

--- main.c
+++ main.c
@@ -2,24 +2,40 @@
 #include <stdio.h>
 #include <string.h>
 
-char verifyAlphabet(char letter,int rotation){
+char verifyAlphabet(int criptValue,char letter,int rotation){
 	char alphabet[26][2] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 	char aux[] = {letter};
 	for(int i=0; i<26;i++){
 		if(strcmp(alphabet[i],aux)==0){
-			printf("Posicao da letra no alfabto: %d\n",i);
+			int alphPosit = i;
+			if(criptValue == 3){
+			    int positRotation = alphPosit+rotation;
+			    if(positRotation > 25){
+			        int pos = (25+(rotation-1)-25);
+			        printf("%s",alphabet[pos]);
+			        break;
+			    }
+			    printf("%s",alphabet[positRotation]);
+			}else{
+			    int positRotation = alphPosit-rotation;
+			    printf("%s",alphabet[positRotation]);
+			}
 		}
 	}
 }
 
 char encrypt(char msg[500],int rotation){
+    int criptValue = 3;
 	for(int i=0; i<strlen(msg); i++){
-		verifyAlphabet(msg[i],rotation);
+	        verifyAlphabet(criptValue,msg[i],rotation);
 	}
 }
 
 char decrypt(char msg[500],int rotation){
-	printf("Funcao decrypt: %s\n",msg);
+    int criptValue = 4;
+	for(int i=0; i<strlen(msg); i++){
+	        verifyAlphabet(criptValue,msg[i],rotation);
+	}
 }
 
 int main(){
@@ -27,22 +43,25 @@ int main(){
 	char message[500]={""};
 	int rotationValue;
 	
-	printf("Enter 1 to encrypt or 2 to decrypt:\t");
+	printf("Enter 1 to encrypt or 2 to decrypt:\n");
+	printf("Digite 1 para encriptar ou 2 para descriptar:\t");
 	scanf("%d",&operOption);
 	
-	printf("\nEnter the message to be operated (JUST UPPERCASE):\n");
+	printf("\nEnter the message to be operated (JUST UPPERCASE)(NO SPACES):\n");
+	printf("Digite a mensagem a ser operada (SOMENTE LETRA MAIUSCULAS)(SEM ESPACOS):\n");
 	scanf("%s",message);
 	
-	printf("\nEnter the rotation value:\t");
+	printf("\nEnter the rotation value:\n");
+	printf("Digite o valor de rotacao:\t");
 	scanf("%d",&rotationValue);
 	
 	if(operOption == 1){
-		printf("\n********ENCRYPT********\n");
+		printf("\n********ENCRYPTED MESSAGE********\n");
 		encrypt(message,rotationValue);
 	}else{
-		printf("\n********DECRYPT********\n");
+		printf("\n********DECRYPTED MESSAGE********\n");
 		decrypt(message,rotationValue);
 	}
 	
-	return 0;
+	getchar();
 }
